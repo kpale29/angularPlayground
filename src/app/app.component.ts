cf39f4c579c1c159583847228d9b0b86fd30b917
Kevin Ricardo Palencia Gil <kevingil1910@gmail.com>
2024-09-06 14:33:50 -0600

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WebcamModule } from 'ngx-webcam';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WebcamModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'testAngular';
+  ngOnInit() {
+    // Al inicializar la aplicación, verifica si está en un navegador embebido
+    if (this.isEmbeddedBrowser()) {
+      this.redirectToBrowser();
+    }
+  }
+
+  // Función que detecta si el usuario está utilizando Facebook o Instagram embebido
+  isEmbeddedBrowser(): boolean {
+    const userAgent = navigator.userAgent || navigator.vendor;
+    return /FBAN|FBAV|Instagram/.test(userAgent);
+  }
+
+  // Redirige al navegador nativo
+  redirectToBrowser() {
+    const isAndroid = /android/i.test(navigator.userAgent);
+    const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+
+    if (isAndroid) {
+      // Intenta abrir el sitio en el navegador nativo de Android (Chrome)
+      window.location.href =
+        'intent://f9tw33nh-4200.use2.devtunnels.ms/#Intent;scheme=https;package=com.android.chrome;end';
+    } else if (isIOS) {
+      // Abre el sitio en Safari en dispositivos iOS
+      window.location.href =
+        'intent://f9tw33nh-4200.use2.devtunnels.ms/#Intent;scheme=https;package=com.android.chrome;end';
+    } else {
+      // Para otros casos, simplemente redirige al sitio
+      window.location.href =
+        'intent://f9tw33nh-4200.use2.devtunnels.ms/#Intent;scheme=https;package=com.android.chrome;end';
+    }
+  }
 }
